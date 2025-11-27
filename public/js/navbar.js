c05e6d995b5921f00e5b5be76c1d61cb2028517c
All4nFernandes <allandossantosfernandes@gmail.com>
2025-11-26 21:39:25 -0400

    refazendo portifolio

--- public/js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("Navbar/navbar.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("Navbar").innerHTML = data;
-    })
-    .catch((error) => console.error("Erro ao carregar a navbar:", error));
-});
